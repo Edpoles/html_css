a0c4b466c7e0d8398a7cf977d4e392026e4d7418
ed <eddy.grosset@lepoles.org>
2022-05-05 15:49:42 +0200

    05052022

---
+++ js/exo04052022.js
@@ -0,0 +1,18 @@
+// var age = prompt("quel âge vous avez?");
+// if (age > 10) {
+//   document.write("Eddy");
+// var monPrenom = prompt("Quel est votre prenom ?");
+// if (monPrenom == "eddy") {
+//   document.write(monPrenom);
+// } else {
+//   document.write("cette valeur n'est pas mon prénom");
+// }
+var unChiffre = prompt("Entrez un chiffre");
+var result = unChiffre % 5;
+if (result == 0) {
+  document.write("divisible par 5");
+} else {
+  document.write("n est pas un multiple de 5");
+}
+
+Number(prompt("Entrez nb1 :"));
---
+++ js/exo05052022.js
@@ -0,0 +1,92 @@
+// let nom = prompt("Fontaine");
+// alert("welcome, vous vous appelez ?" + nom);
+
+// var nom = prompt("votre nom");
+// la valeur sera recupérer dans la boite de dialogue
+// if (nom == 5) {
+//   document.write(nom);
+// }
+// si la valeur est égale à 5 elle sera affiché sur la page
+// l'accolade {} permet d'utiliser une autre fonction
+// nom = parseInt(nom);
+// parseInt (variable converti en Number)
+// console.log(typeof nom);
+// permet d'afficher le type de la valeur pour véfifier si la conversion a été effecuée
+
+// var nombre = prompt("150");
+// var nombreEntier = parseInt(nombre);
+// var resultat = nombreEntier % 13;
+// if (resultat === 0) {
+//   document.write("le resultat est un multiple de 13");
+// } else {
+//   document.write("ce n'est pas un multiple de 13");
+// }
+
+// correction
+// var nombre = prompt ("Entrez votre nombre : ");
+// var nombreFloat = parseFloat (nombre);
+// var result = nombreFloat%13;
+// if (result === 0) {
+// document.write (nombre + "est un multiple de 13");} else {
+// document.write (nombre = "n'est pas un multiple de 13 et le résultat est :" + result);}
+//
+// mon exo =>
+// var nombre = prompt("Lettre ou nombre");
+// var nombre = parseInt(nombre);
+// var result = nombre % 7;
+// if (isNaN(result)) {
+//   document.write(result + "c'est une chaîne de caractére");
+// } else {
+//   document.write(result + "ce n'est pas un multiple de 7");
+// }
+
+// var saisie = prompt("Entrez votre saisie : ");
+// if (isNaN(saisie)) {
+//   document.write(saisie + "est une chaine de caractère composée de chiffres");
+// } else {
+//   var finalResalt = saisie % 7;
+//   if (finalResalt == 0) {
+//     document.write(saisie + " est un multiple de 7");
+//   } else {
+//     document.write(
+//       saisie +
+//         "n'est pas un multiple de 7 et le résulta du modulo : " +
+//         finalResalt
+//     );
+//   }
+// }
+
+// mon exercice
+// var nombre = prompt("tapez");
+// var nombre = parseInt(nombre);
+// var nombre2 = 5;
+// var resultat = nombre * nombre2;
+// document.write(resultat + "<br>");
+// document.write(resultat % 3);
+
+// formule conversion celcuis en fahrenheit
+// °F(°C x 1.8) + 32
+// K = °C + 273.15
+
+// mon exercice
+// var temp = prompt("tapez la valeur de la température");
+// var degres = parseFloat(temp);
+// var fahrenheit = degres + 1.8 * 32;
+// document.write(fahrenheit);
+
+// mon exercice
+// var nb1 = 25;
+// var nb2 = "hotDog";
+// var nb3;
+// console.log("Chiffre1" + nb1);
+// document.write("la valeur de nb1 est égale à" + "&nbsp" + nb1 + "<br>");
+// document.write("la valeur de nb2 est égale à" + "&nbsp" + nb2 + "<br>");
+
+// nb3 = nb1;
+// nb1 = nb2;
+// nb2 = nb3;
+
+// document.write("la valeur de nb1 est égale à" + "&nbsp" + nb1 + "<br>");
+// document.write("la valeur de nb2 est égale à" + "&nbsp" + nb2 + "<br>");
+
+let numérique = 2022;
